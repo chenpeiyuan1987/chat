5827d1a3de31958b9cdfb2e6520c8c2616a089c6
chenpeiyuan1987 <chenpeiyuan1987@163.com>
2019-05-30 01:31:36 +0800

    finish client part

--- common/src/main/java/org/yuan/imooc/common/CloseUtils.java
+++ common/src/main/java/org/yuan/imooc/common/CloseUtils.java
@@ -5,6 +5,9 @@ import java.io.Closeable;
 public class CloseUtils {
 
     public static void close(Closeable... closeables) {
+        if(closeables == null) {
+            return;
+        }
         for(Closeable closeable : closeables) {
             try {
                 closeable.close();
